dda9475a9907ac0b3d9901bac891f7a90c5e31d2
chenjing <chenjinga@bankstell.com>
2022-04-11 10:20:15 +0800

    feat: utils

---
+++ src/utils/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: name
+ * @Date: 2022-04-11 10:18:55
+ * @LastEditors: name
+ * @LastEditTime: 2022-04-11 10:18:56
+ * @FilePath: \my-node-clif:\project\vue-template\src\utils\index.js
+ * @Description: 
+ * @Email: 
+ * Copyright (c) 2022 by 用户/公司名, All Rights Reserved. 
+ */
